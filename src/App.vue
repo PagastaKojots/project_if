6319a91bd5381f0f3c3ebbfa74becdf2b29d9dd1
nlavr <nlavrr@gmail.com>
2016-10-11 21:55:51 +0300

    autorizaacija

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
+  <input v-model="user.name">
+  <input  type="password" v-model="user.pass">
+
+  <button @click="authorize(user.name,user.pass)">Log in</button>   
+ <pre>{{ user | json }}</pre>
 </template>
-
-<script>  
-export default {}
-
+<script>
+export default {
+ data () {
+    return {
+      user: {
+        name: "",
+        pass: ""
+      }
+    }
+  },
+  methods: {
+    authorize (username, password)  {
+      console.log(password)
+      if (  
+      username == "admin"   
+      &&  
+      password == "pass123" 
+      ){  
+        alert("You Are Admin")  
+      }
+    }
+  }
+}
 </script>
 
-<style>   
-</style>
+<style>
